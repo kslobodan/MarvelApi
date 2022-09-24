fdb44dedae4fe24320e2030278a651a76796bf44
Slobodan Komazec <skomazec@xorantech.com>
2022-09-24 20:04:38 +0200

    code refactoring

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,47 +7,24 @@ import * as Notifications from "./notifications";
 
 export const fetchCharacters = (characterNameStartsWith, limitCharNumber) => {
   return async (dispatch) => {
-    const fetchData = async () => {
-      const response = await Services.getCharacters(
+    try {
+      const charactersData = await fetchCharactersData(
         characterNameStartsWith,
         limitCharNumber
       );
 
-      if (!response.ok) throw new Error(Constants.ERROR_FETCHING_CHARACTERS);
-
-      return await response.json();
-    };
-
-    try {
-      const charactersData = await fetchData();
-
       if (!charactersData?.data?.results) {
-        if (charactersData.code === Constants.REQUEST_THROTTLED)
-          Notifications.dispatchTooManyRequests(dispatch);
-        else Notifications.dispatchFatchingCharactersFailed(dispatch);
+        !charactersData?.data?.results &&
+        charactersData.code === Constants.REQUEST_THROTTLED
+          ? Notifications.dispatchTooManyRequests(dispatch)
+          : Notifications.dispatchFatchingCharactersFailed(dispatch);
+
         return;
       }
 
-      if (charactersData.data.results.length === 0) {
-        Notifications.dispatchCharactersListEmpty(dispatch);
-      } else {
-        const loadedCharacters = charactersData.data.results.map(
-          (character) => {
-            return {
-              key: character.id,
-              characterId: character.id,
-              characterName: character.name,
-              imgHref: character.name,
-              imgUrl: character.thumbnail.path,
-              description: character.description,
-              modified: character.modified,
-            };
-          }
-        );
-
-        dispatch(searchCharacterActions.setDoSearch(false));
-        dispatch(charactersActions.setCharactersList(loadedCharacters));
-      }
+      charactersData.data.results.length === 0
+        ? Notifications.dispatchCharactersListEmpty(dispatch)
+        : setCharactersList(dispatch, charactersData);
     } catch (error) {
       Notifications.dispatchFatchingCharactersFailed(dispatch);
     }
@@ -56,42 +33,75 @@ export const fetchCharacters = (characterNameStartsWith, limitCharNumber) => {
 
 export const fetchComics = (characterId) => {
   return async (dispatch) => {
-    const fetchData = async () => {
-      const response = await Services.getComics(characterId);
-
-      if (!response.ok) throw new Error(Constants.ERROR_FETCHING_COMICS);
-
-      return await response.json();
-    };
-
     try {
-      const comicsData = await fetchData();
+      const comicsData = await fetchData(characterId);
 
       if (!comicsData?.data?.results) {
-        if (comicsData.code === Constants.REQUEST_THROTTLED)
-          Notifications.dispatchTooManyRequests(dispatch);
-        else Notifications.dispatchFatchingComicsFailed(dispatch);
+        comicsData.code === Constants.REQUEST_THROTTLED
+          ? Notifications.dispatchTooManyRequests(dispatch)
+          : Notifications.dispatchFatchingComicsFailed(dispatch);
         return;
       }
 
-      if (comicsData.data.results.length === 0) {
-        Notifications.dispatchComicsListEmpty(dispatch);
-      } else {
-        const loadedComics = comicsData.data.results.map((comic) => {
-          return {
-            key: comic.id,
-            title: comic.title,
-            imgHref: comic.name,
-            imgUrl: comic.thumbnail.path,
-            description: comic.description,
-            modified: comic.modified,
-          };
-        });
-
-        dispatch(comicsActions.setComicsList(loadedComics));
-      }
+      comicsData.data.results.length === 0
+        ? Notifications.dispatchComicsListEmpty(dispatch)
+        : setComicsList(dispatch, comicsData);
     } catch (error) {
       Notifications.dispatchFatchingComicsFailed(dispatch);
     }
   };
+};
+
+const setCharactersList = (dispatch, charactersData) => {
+  const loadedCharacters = charactersData.data.results.map((character) => {
+    return {
+      key: character.id,
+      characterId: character.id,
+      characterName: character.name,
+      imgHref: character.name,
+      imgUrl: character.thumbnail.path,
+      description: character.description,
+      modified: character.modified,
+    };
+  });
+
+  dispatch(searchCharacterActions.setDoSearch(false));
+  dispatch(charactersActions.setCharactersList(loadedCharacters));
+};
+
+const fetchCharactersData = async (
+  characterNameStartsWith,
+  limitCharNumber
+) => {
+  const response = await Services.getCharacters(
+    characterNameStartsWith,
+    limitCharNumber
+  );
+
+  if (!response.ok) throw new Error(Constants.ERROR_FETCHING_CHARACTERS);
+
+  return await response.json();
+};
+
+const fetchData = async (characterId) => {
+  const response = await Services.getComics(characterId);
+
+  if (!response.ok) throw new Error(Constants.ERROR_FETCHING_COMICS);
+
+  return await response.json();
+};
+
+const setComicsList = (dispatch, comicsData) => {
+  const loadedComics = comicsData.data.results.map((comic) => {
+    return {
+      key: comic.id,
+      title: comic.title,
+      imgHref: comic.name,
+      imgUrl: comic.thumbnail.path,
+      description: comic.description,
+      modified: comic.modified,
+    };
+  });
+
+  dispatch(comicsActions.setComicsList(loadedComics));
 };
--- src/store/services.js
+++ src/store/services.js
@@ -1,8 +1,9 @@
 import * as Constants from "../constants";
 import * as Helpers from "../helperFunctions";
+// import axios from "axios";
 
 export const getCharacters = (characterNameStartsWith, limitCharNumber) => {
-  return fetch(
+  const result = fetch(
     `https://gateway.marvel.com/v1/public/characters?${Helpers.GetCharacterNameStartWith(
       characterNameStartsWith
     )}${Helpers.GetLimitCharactersNumber(limitCharNumber)}ts=${
@@ -11,7 +12,35 @@ export const getCharacters = (characterNameStartsWith, limitCharNumber) => {
       Constants.HASH
     )}`
   );
+
+  console.log(result);
+
+  return result;
 };
+
+// export const getCharacters = (characterNameStartsWith, limitCharNumber) => {
+//   let responseAxios;
+
+//   axios
+//     .get(
+//       `https://gateway.marvel.com/v1/public/characters?${Helpers.GetCharacterNameStartWith(
+//         characterNameStartsWith
+//       )}${Helpers.GetLimitCharactersNumber(limitCharNumber)}ts=${
+//         process.env.REACT_APP_MARVEL_TS
+//       }&apikey=${process.env.REACT_APP_MARVEL_PUBLIC_KEY}&hash=${Constants.MD5(
+//         Constants.HASH
+//       )}`
+//     )
+//     .then((response) => {
+//       responseAxios = response.data.data.results;
+//       //   console.log(response.data.data.results);
+//     })
+//     .catch((error) => {
+//       console.error("There was an error!", error);
+//     });
+
+//   return responseAxios.json();
+// };
 
 export const getComics = (characterId) => {
   return fetch(
